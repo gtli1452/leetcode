dac12076a396080f4d6507057befb8416ef55514
Tim.Li <silentbrain1452@gmail.com>
2022-01-24 14:28:02 +0800

    Add 2 solutions of Q350

--- c/0350.Intersection Of Two Array II/intersectionOfTwoArray2.c
+++ c/0350.Intersection Of Two Array II/intersectionOfTwoArray2.c
@@ -24,6 +24,12 @@ node_t *creat_node(int key)
     return q;
 }
 
+/*
+ * Use the hash table to store the value and numbers of the value.
+ * Suppose lengths of two arrays are N (nums1) and M (nums2).
+ * Time complexity : O(N + M)
+ * Space complexity: O(N)
+ */
 int *intersect(int *nums1,
                int nums1Size,
                int *nums2,
@@ -74,14 +80,96 @@ int *intersect(int *nums1,
     return ans;
 }
 
+/*
+ * Use an additional array.
+ * Suppose lengths of two arrays are N (nums1) and M (nums2).
+ * Time complexity : O(1001)
+ * Space complexity: O(N)
+ */
+int *intersect_v2(int *nums1,
+                  int nums1Size,
+                  int *nums2,
+                  int nums2Size,
+                  int *returnSize)
+{
+    *returnSize = 0;
+    int size = nums1Size < nums2Size ? nums1Size : nums2Size;
+    int *ans = (int *) calloc(size, sizeof(int));
+
+    /*
+     * There are some constraints to the arrays:
+     * 1 <= nums1.length, nums2.length <= 1000
+     * 0 <= nums1[i], nums2[i] <= 1000
+     *
+     * We can use the value as an index to record the numbers of the same value.
+     */
+    int tmp[1001] = {0};
+    for (int i = 0; i < nums1Size; i++) {
+        tmp[nums1[i]]++;
+    }
+
+    for (int i = 0; i < nums2Size; i++) {
+        if (tmp[nums2[i]]) {
+            tmp[nums2[i]]--;
+            ans[(*returnSize)++] = nums2[i];
+        }
+    }
+
+    return ans;
+}
+
+static int cmp(const void *lhs, const void *rhs)
+{
+    if (*(int *) lhs == *(int *) rhs)
+        return 0;
+
+    return *(int *) lhs < *(int *) rhs ? -1 : 1;
+}
+
+/*
+ * Sorting the arrays then using the Two-Pointers.
+ * Suppose lengths of two arrays are N (nums1) and M (nums2).
+ * Time complexity : O(N logN) + O(M logM); worst is O(N^2) + O(M^2)
+ * Space complexity: O(min(N, M))
+ */
+int *intersect_v3(int *nums1,
+                  int nums1Size,
+                  int *nums2,
+                  int nums2Size,
+                  int *returnSize)
+{
+    *returnSize = 0;
+    int size = nums1Size < nums2Size ? nums1Size : nums2Size;
+    int *ans = (int *) calloc(size, sizeof(int));
+
+    qsort(nums1, nums1Size, sizeof(nums1[0]), cmp);
+    qsort(nums2, nums2Size, sizeof(nums2[0]), cmp);
+
+    int i = 0, j = 0;
+
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            ans[(*returnSize)++] = nums1[i];
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     int nums1[] = {1, 2, 2, 1};
     int nums2[] = {2, 2};
     int *returnSize;
 
-    int *ans = intersect(nums1, sizeof(nums1) / sizeof(nums1[0]), nums2,
-                         sizeof(nums2) / sizeof(nums2[0]), returnSize);
+    int *ans = intersect_v3(nums1, sizeof(nums1) / sizeof(nums1[0]), nums2,
+                            sizeof(nums2) / sizeof(nums2[0]), returnSize);
 
     for (int i = 0; i < *returnSize; i++)
         printf("%d, ", ans[i]);
